86feec8a203ea5f233a565ff3a5f833399a4d021
Henri Verroken <henriverroken@gmail.com>
2016-11-08 18:27:23 +0000

    Implement build command to parse and print lxdfiles

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
+
+import Control.Monad.Except (ExceptT, MonadError, runExceptT, throwError)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 
 import Options.Applicative
 
+import System.Exit (exitFailure)
+
+import Language.LXDFile (parseFile)
 
 data Command = BuildCommand FilePath String FilePath -- ^ LXDFile, image tag and base directory
+
+newtype CmdT m a = CmdT { runCmdT :: ExceptT String m a }
+                 deriving (Functor, Applicative, Monad, MonadIO, MonadError String)
 
 buildCmd :: Mod CommandFields Command
 buildCmd =
@@ -16,11 +27,26 @@ buildCmd =
 subcommand :: Parser Command
 subcommand = subparser buildCmd
 
-
 main :: IO ()
 main =
     execParser opts >>= run
   where
     opts = info (helper <*> subcommand) $ progDesc "Automatically build and manage LXD images and containers."
-    run (BuildCommand lxdfile tag base) = putStrLn $ "Build " ++ lxdfile ++ " " ++ tag ++ " " ++ base
+    run (BuildCommand lxdfile tag base) = cmd $ build lxdfile tag base
 
+cmd :: CmdT IO () -> IO ()
+cmd action = do
+    x <- runExceptT $ runCmdT action
+    case x of Right () -> return ()
+              Left e -> do
+                putStrLn $ "error: " ++ e
+                exitFailure
+
+build :: (MonadIO m, MonadError String m) => FilePath -> String -> FilePath -> m ()
+build fp name dir = do
+    lxdfile <- liftIO (parseFile fp) >>= orErr "parse error"
+    liftIO $ putStrLn $ "Building " ++ name ++ " with " ++ fp ++ " using context " ++ dir
+    liftIO $ print lxdfile
+  where
+    orErr pref = either (showErr pref) return
+    showErr pref e = throwError $ pref ++ ": " ++ show e
